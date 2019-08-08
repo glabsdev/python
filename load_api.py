1a4efa7900c07dc6c0e6995f3b3beb49a93b0a38
glabsdev <53467246+glabsdev@users.noreply.github.com>
2019-08-08 15:54:40 +0800

    Add files via upload

--- load_api.py
+++ load_api.py
@@ -1,6 +1,13 @@
 import requests
+
+app_id = 'B54z9Ug5512HHrTGRT5g6hq64pGUq6ap'
+app_secret = 'f655413712345607a696cd40741993758c411af3bb5f6c230270ec26e8d54126'
+rewards_token = 'AbCkxKYid_F_p-JSgTejow'
+address = '9001234567'
+promo = 'LOAD 1'
+
 url = "https://devapi.globelabs.com.ph/rewards/v1/transactions/send"
-payload = "{\n \"outboundRewardRequest\" : {\n \"app_id\" : \"<app_id>\",\n \"app_secret\" : \"<app_secret> \",\n \"rewards_token\" : \"<rewards_token> \",\n \"address\" : \"<address>\",\n \"promo\" : \"<promo>\"\n }\n}"
+payload = "{ \"outboundRewardRequest\" : { \"app_id\" : \""+app_id+"\", \"app_secret\" : \""+app_secret+"\", \"rewards_token\" : \""+rewards_token+"\", \"address\" : \""+address+"\", \"promo\" : \""+promo+"\" } }"
 headers = {
     'Content-Type': "application/json",
     'Host': "devapi.globelabs.com.ph"
--- load_api_status.py
+++ load_api_status.py
@@ -1,8 +1,12 @@
 import requests
+
+app_id = 'B54z9Ug5512HHrTGRT5g6hq64pGUq6ap'
+app_secret = 'f655413712345607a696cd40741993758c411af3bb5f6c230270ec26e8d54126'
+rewards_token = 'AbCkxKYid_F_p-JSgTejow'
+transaction_id = '1234567'
+
 url = "https://devapi.globelabs.com.ph/rewards/v1/transactions/status"
-payload = " {\"rewardStatusRequest\" : {\"app_id\" : \"<app_id>\", \"app_secret\" : \"<app_secret> \", \"rewards_token\" : \"<rewards_token> \", \"transaction_id\" : \"<transaction_id>\"} }"
-headers = {
-    'Content-Type': "application/json"
-    }
+payload = "{\"rewardStatusRequest\" : {\"app_id\" : \""+app_id+"\", \"app_secret\" : \""+app_secret+"\", \"rewards_token\" : \""+rewards_token+"\", \"transaction_id\" : \""+transaction_id+"\" } }"
+headers = { 'Content-Type': "application/json" }
 response = requests.request("POST", url, data=payload, headers=headers)
 print(response.text)
