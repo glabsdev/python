c492ff846ab014712ada8c495da2c801006060b4
glabsdev <53467246+glabsdev@users.noreply.github.com>
2019-08-08 12:31:44 +0800

    Add files via upload

--- lbs.py
+++ lbs.py
@@ -1,6 +1,11 @@
 import requests
+
+access_token = 'A-2szoYus7mB13l5axDrr_1234AApSz8eu236GRNsoBQ'
+address = '9771234567'
+requestedAccuracy = '100'
+
 url = "https://devapi.globelabs.com.ph/location/v1/queries/location"
-querystring = {"access_token":"<access_token>","address":"<address>","requestedAccuracy":"100"}
+querystring = {"access_token":access_token,"address":address,"requestedAccuracy":requestedAccuracy}
 headers = {
     'Content-Type': "application/json",
     'Host': "devapi.globelabs.com.ph"
--- sms-mt.py
+++ sms-mt.py
@@ -1,16 +1,16 @@
 import requests
 
-v_shortcode = '2648'
-v_access_token = 'A-2szoYus7mB13l5axDrr_1234AApSz8eu236GRNsoBQ'
-v_address = '9771234567'
-v_clientCorrelator = '268401'
-v_message = 'Python SMS Test'
+shortcode = '2648'
+access_token = 'A-2szoYus7mB13l5axDrr_1234AApSz8eu236GRNsoBQ'
+address = '9771234567'
+clientCorrelator = '268401'
+message = 'Python SMS Test'
 
-url = "https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/"+v_shortcode+"/requests"
+url = "https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/"+shortcode+"/requests"
 
-querystring = {"access_token":v_access_token}
+querystring = {"access_token":access_token}
 
-payload = "{\"outboundSMSMessageRequest\": { \"clientCorrelator\": \""+v_clientCorrelator+"\", \"senderAddress\": \""+v_shortcode+"\", \"outboundSMSTextMessage\": {\"message\": \""+v_message+"\"}, \"address\": \""+v_address+"\" } }"
+payload = "{\"outboundSMSMessageRequest\": { \"clientCorrelator\": \""+clientCorrelator+"\", \"senderAddress\": \""+shortcode+"\", \"outboundSMSTextMessage\": {\"message\": \""+message+"\"}, \"address\": \""+address+"\" } }"
 headers = { 'Content-Type': "application/json", 'Host': "devapi.globelabs.com.ph" }
 
 response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
